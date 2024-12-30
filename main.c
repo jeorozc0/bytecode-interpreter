cc13ee942f94d980467cc22cea41ef3fc5aa1fa5
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-12-30 10:39:55 -0500

    feat: add debugger function call

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "chunk.h"
-#include "common.h"
+#include "debug.h"
 
 int main(int argc, const char * argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+
+    //Given a chunk, this will print out the instructions
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
 }
