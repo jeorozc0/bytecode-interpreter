e70438d4ada70bbe3aca209149ea353161de05b2
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-12-30 12:04:23 -0500

    feat: implement function to disassemble over each instruction in the bytecode

--- debug.c
+++ debug.c
@@ -14,3 +14,20 @@ void disassembleChunk(Chunk* chunk, const char* name) {
         offset = disassembleInstruction(chunk, offset);
     }
 }
+ int disassembleInstruction(Chunk* chunk, int offset) {
+    //Prints byte offset of the instruction
+    printf("%04d ", offset);
+
+    //Reads single byte from the bytecode at given offset
+    uint8_t instruction = chunk->code[offset];
+
+    /* We switch on the given instruction, printing any bug
+     * in the compiler*/
+    switch (instruction) {
+        case OP_RETURN:
+            return simpleInstruction('OP_RETURN', offset);
+        default:
+            printf("Unknown opcode %d\n", instruction);
+        return offset + 1;
+    }
+}
