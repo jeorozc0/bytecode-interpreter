e42c4bbb94e7a10c2945f4ccda7ac754cb7eaaaa
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-12-30 14:01:54 -0500

    feat: implement line number assignment

--- chunk.c
+++ chunk.c
@@ -8,6 +8,7 @@ void initChunk(Chunk *chunk) {
     chunk -> count = 0;
     chunk -> capacity = 0;
     chunk -> code = NULL;
+    chunk -> lines = NULL;
     initValueArray(&chunk->constants);
 }
 
@@ -15,19 +16,22 @@ void initChunk(Chunk *chunk) {
  and leave the chunk in a well-defined empty state */
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity =
             GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
