c3321abc18751e5102f19768b80bbd0b5ff71d50
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2024-12-30 12:12:42 -0500

    fix: fix name of definition in debug header file

--- debug.h
+++ debug.h
@@ -1,9 +1,9 @@
-#ifndef DEBUG_H
-#define DEBUG_H
+#ifndef clox_debug_h
+#define clox_debug_h
 
 #include "chunk.h"
 
 void disassembleChunk(Chunk* chunk, const char* name);
 int disassembleInstruction(Chunk* chunk, int offset);
 
-#endif //DEBUG_H
+#endif
