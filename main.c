a745cfedce451721198cbcf3ae6c5b496ef8514e
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-06 15:46:48 -0500

    feat: add conditionals to read arguments to start interpreter

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
-#include "chunk.h"
-#include "debug.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
 #include "vm.h"
 
 int main(int argc, const char * argv[]) {
     initVM();
+
+    if (argc == 1) {
+        repl();
+    } else if (argc ==  2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: clox [path]\n");
+        exit(64);
+    }
 
     freeVM();
     return 0;
