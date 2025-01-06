0e5989430339e7ab1abf01b1f941c6dca5a94bb4
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-06 15:47:03 -0500

    feat: add function to safely read files

--- main.c
+++ main.c
@@ -5,6 +5,63 @@
 #include "common.h"
 #include "vm.h"
 
+static void repl() {
+    //For now, we have fixed number of lines
+    char line[1024];
+    for (;;) {
+        printf("> ");
+
+        if (fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path) {
+    // Opens file in binary read mode
+    FILE* file = fopen(path, "rb");
+    //File doesn't exist or user doesn't have access to it
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\".\n", path);
+        exit(74);
+    }
+
+    // Gets file size:
+    fseek(file, 0L, SEEK_END);    // Move to end
+    size_t fileSize = ftell(file); // Get position (size)
+    rewind(file);                  // Back to start
+
+
+    // Allocate buffer with space for null terminator
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    // Read entire file into buffer
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead<fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+    }
+    buffer[bytesRead] = '\0';      // Add null terminator
+
+    fclose(file);
+    return buffer;                 // Caller must free this memory
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR) exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
 int main(int argc, const char * argv[]) {
     initVM();
 
