40a14109581eb0f8217ab03c078f23bcfc3ad613
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-06 16:49:31 -0500

    feat: start scanner

--- scanner.c
+++ scanner.c
@@ -9,3 +9,11 @@ typedef struct {
     const char* current;
     int line;
 } Scanner;
+
+Scanner scanner;
+
+void initScanner(const char* source) {
+    scanner.start = source;
+    scanner.current = source;
+    scanner.line = 1;
+}
