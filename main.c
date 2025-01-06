d8449255b2d0ad811586b2fa7430a4501b421010
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-06 15:14:48 -0500

    misc: clear main func

--- main.c
+++ main.c
@@ -5,20 +5,6 @@
 int main(int argc, const char * argv[]) {
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
-
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    writeChunk(&chunk, OP_RETURN, 123);
-
-    //Given a chunk, this will print out the instructions
-    disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
     freeVM();
-    freeChunk(&chunk);
     return 0;
 }
