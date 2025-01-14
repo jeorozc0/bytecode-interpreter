7a8af8d307d5820955964edec82db6c6037ffa09
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-14 14:18:37 -0500

    feat: create function to scan tokens correctly

--- scanner.c
+++ scanner.c
@@ -5,15 +5,45 @@
 #include "scanner.h"
 
 typedef struct {
-    const char* start;
-    const char* current;
+    const char *start;
+    const char *current;
     int line;
 } Scanner;
 
 Scanner scanner;
 
-void initScanner(const char* source) {
+void initScanner(const char *source) {
     scanner.start = source;
     scanner.current = source;
     scanner.line = 1;
+}
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int) (scanner.current - scanner.start);
+    //Uses the scanner’s start and current pointers to capture the token’s lexeme
+    token.line = scanner.line;
+    return token;
+}
+
+static Token errorToken(const char *message) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int) strlen(message);
+    token.line = scanner.line;
+    return token;
+}
+
+Token scanToken() {
+    scanner.start = scanner.current;
+    //Function always scan a complete token, so we are always at beginning when we enter function
+    if (isAtEnd()) return makeToken(TOKEN_EOF);
+    return errorToken("Unexpected character.");
 }
