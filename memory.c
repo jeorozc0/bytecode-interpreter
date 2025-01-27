7b5d067817032d29887a9fbaa8c2ae244c06b98d
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-27 17:18:19 -0500

    add free method for each object in memory list

--- memory.c
+++ memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "memory.h"
+#include "object.h"
 #include "value.h"
 #include "vm.h"
 
@@ -13,6 +14,17 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
   if (result == NULL)
     exit(1);
   return result;
+}
+
+static void freeObject(Obj *object) {
+  switch (object->type) {
+  case OBJ_STRING: {
+    ObjString *string = (ObjString *)object;
+    FREE_ARRAY(char, string->chars, string->length + 1);
+    FREE(ObjString, object);
+    break;
+  }
+  }
 }
 
 void freeObjects() {
