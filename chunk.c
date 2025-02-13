d9eeb5c30addfd874cf6994ba7c8c05c3f4baba7
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-02-13 11:58:57 -0500

    refactor chunk file and delete reference file

--- chunk.c
+++ chunk.c
@@ -24,8 +24,7 @@ void freeChunk(Chunk* chunk) {
 void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
-        chunk->capacity =
-            GROW_CAPACITY(oldCapacity);
+        chunk->capacity=GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
         chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
