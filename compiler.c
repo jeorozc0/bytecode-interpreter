723e29be374caccde22ed66a76b91b1086de7b9c
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-02-17 17:05:09 -0500

    add function declaration support

--- compiler.c
+++ compiler.c
@@ -330,6 +330,8 @@ static uint8_t parseVariable(const char *errorMessage) {
 }
 
 static void markInitialized() {
+  if (current->scopeDepth == 0)
+    return;
   current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
@@ -419,6 +421,46 @@ static void block() {
   }
 
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+// Compiles a function definition into bytecode
+static void function(FunctionType type) {
+  // Create and initialize a new compiler for this function
+  Compiler compiler;
+  initCompiler(&compiler, type);
+
+  // Create a new scope for function parameters and body
+  beginScope();
+
+  // Parse function syntax
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+  // Here we'll later add parameter parsing
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+
+  // Compile the function body
+  block();
+
+  // Finish compilation and get the resulting function object
+  ObjFunction *function = endCompiler();
+
+  // Emit the function object as a constant in the enclosing function's chunk
+  emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+}
+
+// Compiles a function declaration statement
+static void funcDeclaration() {
+  // Parse and declare the function's name as a global variable
+  uint8_t global = parseVariable("Expect function name.");
+
+  // Mark variable as initialized right away so function can be recursive
+  markInitialized();
+
+  // Compile the function body
+  function(TYPE_FUNCTION);
+
+  // Define the global variable that holds the function
+  defineVariable(global);
 }
 
 static void varDeclaration() {
@@ -590,14 +632,16 @@ static void synchronize() {
 }
 
 static void declaration() {
-  if (match(TOKEN_VAR)) {
+  if (match(TOKEN_FUN)) {
+    funcDeclaration();
+  } else if (match(TOKEN_VAR)) {
     varDeclaration();
   } else {
     statement();
   }
 
-  // if we hit a compile error while parsing the previous statement, enter panic
-  // mode after the erroneous statement, start synchronizing
+  // if we hit a compile error while parsing the previous statement, enter
+  // panic mode after the erroneous statement, start synchronizing
   if (parser.panicMode)
     synchronize();
 }
@@ -644,7 +688,8 @@ static void or_(bool canAssign) {
   // Second jump: if condition is true, skip second operand
   int endJump = emitJump(OP_JUMP);
 
-  // Patch the first jump to point here (where second operand evaluation begins)
+  // Patch the first jump to point here (where second operand evaluation
+  // begins)
   patchJump(elseJump);
 
   // Pop the false value before evaluating second operand
@@ -680,8 +725,8 @@ static void namedVariable(Token name, bool canAssign) {
     setOp = OP_SET_GLOBAL;
   }
 
-  // look for an equal sign after identifier, to determine whether this is a set
-  // or a get
+  // look for an equal sign after identifier, to determine whether this is a
+  // set or a get
   if (canAssign && match(TOKEN_EQUAL)) {
     expression();
     emitBytes(setOp, (uint8_t)arg);
@@ -756,12 +801,12 @@ ParseRule rules[] = {
     [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
 };
 
-// start at the current token, and parses any expression at the given precedence
-// level or higher refer to Section 17.6.1 for details
+// start at the current token, and parses any expression at the given
+// precedence level or higher refer to Section 17.6.1 for details
 static void parsePrecedence(Precedence precedence) {
   advance();
-  // first token is always going to belong to some kind of prefix expression, by
-  // definition
+  // first token is always going to belong to some kind of prefix expression,
+  // by definition
   ParseFn prefixRule = getRule(parser.previous.type)->prefix;
   if (prefixRule == NULL) {
     error("Expect expression.");
@@ -771,8 +816,8 @@ static void parsePrecedence(Precedence precedence) {
   // only allow assignment when parsing an assignment expression
   // or top-level expression like in an expression statement
   // fixes `a * b = c + d`, see Section 21.4
-  // if the variable is nested in higher precedence expression, `canAssign` will
-  // be false, and `=` will be ignored
+  // if the variable is nested in higher precedence expression, `canAssign`
+  // will be false, and `=` will be ignored
   bool canAssign = precedence <= PREC_ASSIGNMENT;
   prefixRule(canAssign);
 
@@ -786,8 +831,8 @@ static void parsePrecedence(Precedence precedence) {
   }
 
   // `=` doesn't get consumed, indicating in further parsing the precedence
-  // isn't low enough (See section 21.4) if `canAssign` is true, and assignment
-  // is allowed, `=` should get consumed
+  // isn't low enough (See section 21.4) if `canAssign` is true, and
+  // assignment is allowed, `=` should get consumed
   if (canAssign && match(TOKEN_EQUAL)) {
     error("Invalid assignment target.");
   }
